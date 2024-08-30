5ca1679475de7f157d87b235301fbb380879c96e
initial-mockingbird <daniel.andres.pinto@gmail.com>
2024-08-30 03:10:37 -0400

    adding descriptions to haskell commands

--- config/lang/haskell-tools.nix
+++ config/lang/haskell-tools.nix
@@ -1,29 +1,30 @@
-{ pkgs, ... }: 
+{ pkgs, mlKey, ... }: 
 {
   extraPlugins = with pkgs.vimPlugins; [
     haskell-tools-nvim	
   ];
 
-  extraConfigLua = ''
-      -- ~/.config/nvim/after/ftplugin/haskell.lua
-      local ht = require('haskell-tools')
-      local bufnr = vim.api.nvim_get_current_buf()
-      local opts = { noremap = true, silent = true, buffer = bufnr, }
-      -- haskell-language-server relies heavily on codeLenses,
-      -- so auto-refresh (see advanced configuration) is enabled by default
-      vim.keymap.set('n', '<space>cl', vim.lsp.codelens.run, opts)
-      -- Hoogle search for the type signature of the definition under the cursor
-      vim.keymap.set('n', '<space>hs', ht.hoogle.hoogle_signature, opts)
-      -- Evaluate all code snippets
-      vim.keymap.set('n', '<space>ea', ht.lsp.buf_eval_all, opts)
-      -- Toggle a GHCi repl for the current package
-      vim.keymap.set('n', '<leader>rr', ht.repl.toggle, opts)
-      -- Toggle a GHCi repl for the current buffer
-      vim.keymap.set('n', '<leader>rf', function()
-        ht.repl.toggle(vim.api.nvim_buf_get_name(0))
-      end, opts)
-      vim.keymap.set('n', '<leader>rq', ht.repl.quit, opts)
-      require('telescope').load_extension('ht')
+    extraConfigLua = ''
+    local ht = require('haskell-tools')
+    local bufnr = vim.api.nvim_get_current_buf()
+    function mkOpts(desc)
+      return { noremap = true, silent = true, buffer = bufnr, desc = desc}
+    end
+  -- haskell-language-server relies heavily on codeLenses,
+    -- so auto-refresh (see advanced configuration) is enabled by default
+    vim.keymap.set('n', '<leader>cl', vim.lsp.codelens.run, mkOpts('Haskell Run Codelens') )
+    -- Hoogle search for the type signature of the definition under the cursor
+    vim.keymap.set('n', '<leader>hs', ht.hoogle.hoogle_signature, mkOpts('Hoogle Search Word'))
+    -- Evaluate all code snippets
+    vim.keymap.set('n', '<leader>ea', ht.lsp.buf_eval_all, mkOpts('Eval all code snippets'))
+    -- Toggle a GHCi repl for the current package
+    vim.keymap.set('n', '<leader>rr', ht.repl.toggle, mkOpts('GHCI current package'))
+    -- Toggle a GHCi repl for the current buffer
+    vim.keymap.set('n', '<leader>rf', function()
+      ht.repl.toggle(vim.api.nvim_buf_get_name(0))
+    end, mkOpts('GHCI for current buffer')
+    vim.keymap.set('n', '<leader>rq', ht.repl.quit, mkOpts('Quit repl'))
+    require('telescope').load_extension('ht')
     '';
 
 }
